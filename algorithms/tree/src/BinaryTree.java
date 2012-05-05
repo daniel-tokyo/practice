63cdbb495c9eda3c6f5b7f69716ac948459dc7bb
embira <embira@gmail.com>
2012-05-06 00:11:42 +0900

    * Splite ToStringHelper to a separeted file.

--- algorithms/tree/src/BinaryTree.java
+++ algorithms/tree/src/BinaryTree.java
@@ -63,30 +63,6 @@ public abstract class BinaryTree {
     }
 }
 
-class ToStringHelper implements BinaryTreeHelper {
-
-    // private members
-
-    private StringBuffer m_oStrBuffer;
-
-    // constructors
-
-    public ToStringHelper() {
-        m_oStrBuffer = new StringBuffer();
-    }
-
-    // public methods
-
-    // implemnts the interface of BinaryTreeHelper
-    public void treat(BinaryTreeNode oNode) {
-        m_oStrBuffer.append(oNode.toString() + " ");
-    }
-
-    public String toString() {
-        return m_oStrBuffer.toString().trim();
-    }
-}
-
 /*{+----------------------------------------- Embira Footer 1.6 ---------+
    | vim<600:set et sw=4 ts=4 sts=4:                                     |
    | vim600:set et sw=4 ts=4 sts=4 ff=unix cindent fdm=marker fmr={,}:   |
---
+++ algorithms/tree/src/ToStringHelper.java
@@ -0,0 +1,34 @@
+/**
+ * Created at: 2012-05-06 00:05:34
+ *
+ * $Id:$
+ */
+
+public class ToStringHelper implements BinaryTreeHelper {
+
+    // private members
+
+    private StringBuffer m_oStrBuffer;
+
+    // constructors
+
+    public ToStringHelper() {
+        m_oStrBuffer = new StringBuffer();
+    }
+
+    // public methods
+
+    // implemnts the interface of BinaryTreeHelper
+    public void treat(BinaryTreeNode oNode) {
+        m_oStrBuffer.append(oNode.toString() + " ");
+    }
+
+    public String toString() {
+        return m_oStrBuffer.toString().trim();
+    }
+}
+
+/*{+----------------------------------------- Embira Footer 1.6 ---------+
+   | vim<600:set et sw=4 ts=4 sts=4:                                     |
+   | vim600:set et sw=4 ts=4 sts=4 ff=unix cindent fdm=marker fmr={,}:   |
+   +---------------------------------------------------------------------+}*/
