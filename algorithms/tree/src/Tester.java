0b851eb26378b9605146ec2e0894e42baa32bba3
daniel <cd@accessport.jp>
2012-05-16 18:03:12 +0900

    * Add integer, float, double tree test

--- algorithms/tree/src/Tester.java
+++ algorithms/tree/src/Tester.java
@@ -8,12 +8,11 @@ import java.util.Random;
 
 public class Tester {
 
+    private static BinaryTree<?>        m_oTree = null;
     private static int                  m_iCount = 100;
-    private static Tracer               m_oTracer = new Tracer();
-
     private static TreeFactory.TreeType m_eType = TreeFactory.TreeType.DEFAULT;
     private static String               m_sURL = new String();
-    private static TreeFactory          m_oTreeFactory = new TreeFactory(m_oTracer); 
+    private static Tracer               m_oTracer = new Tracer();
 
     public static void main(String[] args) {
         try { 
@@ -21,11 +20,8 @@ public class Tester {
                 usage();
             } else {
                 m_oTracer.echo("Testing for binary search tree: ");
-                m_oTracer.echo("----- build tree -----");
-                BinarySearchTree<?> oTree = m_oTreeFactory.build(m_eType, 
-                                                                 m_iCount);
-                m_oTracer.echo("----- output -----");
-                output(oTree);
+                build();
+                output();
                 m_oTracer.echo("----- END -----");
             } 
         } catch (Exception e) {
@@ -67,9 +63,22 @@ public class Tester {
         return bRet;
     }
 
-    private static void output(BinarySearchTree<?> oTree) {
-        if (oTree != null) {
-            m_oTracer.echo("[" + oTree + "]");
+    private static boolean build() {
+        m_oTracer.echo("----- build tree -----");
+        if (m_oTree == null) {
+            TreeFactory oTreeFactory = new TreeFactory(m_oTracer); 
+            m_oTree = oTreeFactory.build(m_eType, m_iCount);
+            return (m_oTree != null);
+        } else {
+            m_oTracer.echo("WARNING: The tree exists!");
+            return false;
+        }
+    }
+
+    private static void output() {
+        m_oTracer.echo("----- output -----");
+        if (m_oTree != null) {
+            m_oTracer.echo("[" + m_oTree + "]");
         } else {
             m_oTracer.echo("The tree is empty!");
         }
@@ -88,34 +97,30 @@ class TreeFactory {
         DEFAULT, INTEGER, FLOAT, DOUBLE, STRING
     }
 
-    public BinarySearchTree build(TreeType eType, int iCnt) {
+    public BinaryTree<?> build(TreeType eType, int iCnt) {
         switch (eType) {
             case INTEGER:
-                return buildInteger(iCnt);
+                return buildIntegerTree(iCnt);
             case FLOAT:
-                return buildFloat(iCnt);
+                return buildFloatTree(iCnt);
             case DOUBLE:
-                return buildDouble(iCnt);
+                return buildDoubleTree(iCnt);
             case STRING:
-                return buildString(iCnt);
+                //return buildStringTree(iCnt);
             default:
                 assert false; // ERROR: The tree type is wrong!
                 return null;
         }
     }
 
-    private BinarySearchTree<?> buildInteger(int iCnt) {
-        return null;
-    }
-
-    private BinarySearchTree<?> buildFloat(int iCnt) {
-        BinarySearchTree<Float> oTree = new BinarySearchTree<Float>();
+    private BinaryTree<?> buildIntegerTree(int iCnt) {
+        BinarySearchTree<Integer> oTree = new BinarySearchTree<Integer>();
         if (oTree != null) {
             Random oRandom = new Random();
             for (int i = 0; i < iCnt; i++) {
-                Float ofKey = new Float(oRandom.nextInt(10000) + oRandom.nextFloat());
-                String oMsg = "Add [" + ofKey + "]...(" + i + ")";
-                if (oTree.add(ofKey) == false) {
+                Integer oKey = new Integer(oRandom.nextInt(10000));
+                String oMsg = "Add [" + oKey + "]...(" + i + ")";
+                if (oTree.add(oKey) == false) {
                     oMsg +=  " -> failed!";
                 }
                 m_oTracer.echo(oMsg);
@@ -124,12 +129,36 @@ class TreeFactory {
         return oTree;
     }
 
-    private BinarySearchTree<?> buildDouble(int iCnt) {
-        return null;
+    private BinaryTree<?> buildFloatTree(int iCnt) {
+        BinarySearchTree<Float> oTree = new BinarySearchTree<Float>();
+        if (oTree != null) {
+            Random oRandom = new Random();
+            for (int i = 0; i < iCnt; i++) {
+                Float oKey = new Float(oRandom.nextInt(10000) + oRandom.nextFloat());
+                String oMsg = "Add [" + oKey + "]...(" + i + ")";
+                if (oTree.add(oKey) == false) {
+                    oMsg +=  " -> failed!";
+                }
+                m_oTracer.echo(oMsg);
+            }
+        }
+        return oTree;
     }
 
-    private BinarySearchTree<?> buildString(int iCnt) {
-        return null;
+    private BinaryTree<?> buildDoubleTree(int iCnt) {
+        BinarySearchTree<Double> oTree = new BinarySearchTree<Double>();
+        if (oTree != null) {
+            Random oRandom = new Random();
+            for (int i = 0; i < iCnt; i++) {
+                Double oKey = new Double(oRandom.nextInt(10000) + oRandom.nextDouble());
+                String oMsg = "Add [" + oKey + "]...(" + i + ")";
+                if (oTree.add(oKey) == false) {
+                    oMsg +=  " -> failed!";
+                }
+                m_oTracer.echo(oMsg);
+            }
+        }
+        return oTree;
     }
 }
 
